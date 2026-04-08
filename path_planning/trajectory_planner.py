d3661ba1fc16251ace12caf085764fdfed693489
kareena-s <kareenas@mit.edu>
2026-04-08 03:22:09 +0000

    prelim sample planning code

--- path_planning/trajectory_planner.py
+++ path_planning/trajectory_planner.py
@@ -4,7 +4,7 @@ from geometry_msgs.msg import PoseArray, PoseStamped
 from nav_msgs.msg import OccupancyGrid, Odometry
 from path_planning.utils import LineTrajectory
 from rclpy.node import Node
-
+import numpy as np
 
 class PathPlan(Node):
     """ Listens for goal pose published by RViz and uses it to plan a path from
@@ -45,20 +45,52 @@ class PathPlan(Node):
             10
         )
 
+        self.map_data = None
+        self.map_resolution = None
+        self.map_origin = None
+        self.current_pose = None
+
         self.trajectory = LineTrajectory(node=self, viz_namespace="/planned_trajectory")
 
     def map_cb(self, msg):
-        raise NotImplementedError
+        self.map_data = np.array(msg.data).reshape((msg.info.height, msg.info.width))
+        self.map_resolution = msg.info.resolution
+        self.map_origin = (msg.info.origin.position.x, msg.info.origin.position.y)
 
     def pose_cb(self, pose):
-        raise NotImplementedError
+        self.current_pose = (pose.pose.pose.position.x, pose.pose.pose.position.y)
 
     def goal_cb(self, msg):
-        raise NotImplementedError
+        if self.map_data is None or self.current_pose is None:
+            self.get_logger().warn("Waiting for map or pose")
+
+        goal = (msg.pose.position.x, msg.pose.position.y)
+
+        self.plan_path(self.current_pose, goal, self.map_data)
 
     def plan_path(self, start_point, end_point, map):
+        path = self.rrt(start_point, end_point, map)
+
+        if path is None:
+            self.get_logger().warn("Failed to find a path")
+            return
+        
+        self.trajectory.clear()
+
+        for p in path:
+            self.trajectory.addPoint(p[0], p[1])
+
         self.traj_pub.publish(self.trajectory.toPoseArray())
         self.trajectory.publish_viz()
+
+        self.get_logger().info("Path published")
+
+    def rrt(self, start_point, end_point, map):
+        
+        nodes = [start_point]
+
+
+    
 
 
 def main(args=None):
